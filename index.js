a82f96b1ccbccc6d6c9a637b266f0ec7632d9308
FelixFromDiscord <sidorovf996@gmail.com>
2022-12-03 17:39:29 +0300

    test

--- index.js
+++ index.js
@@ -3,8 +3,25 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
+app.post('/', (req, res) => {
+    res.send(```
+    {
+        "response": {
+          "text": "Здравствуйте! Это мы, хороводоведы.",
+          "tts": "Здравствуйте! Это мы, хоров+одо в+еды.",
+          "buttons": [
+              {
+                  "title": "Надпись на кнопке",
+                  "payload": {},
+                  "url": "https://example.com/",
+                  "hide": false
+              }
+          ],
+          "end_session": false
+        },
+        "version": "1.0"
+      }
+      ```);
 });
 
 app.listen(PORT, () => {
